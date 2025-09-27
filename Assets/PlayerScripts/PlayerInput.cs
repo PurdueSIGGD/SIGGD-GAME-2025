a458d0906aa258ce34e7a75b0df69ee706e40cc7
Bode Lash <lash16@purdue.edu>
2025-09-26 20:36:34 -0400

    Player Input Script + InputSystem_Actions C# Script
    I created a PlayerInput Script inside a PlayerScripts folder. This script holds functions detecting when player inputs are given and has public variables describing move and look inputs. I also generated a C# file for InputSystem_Actions.

---
+++ Assets/PlayerScripts/PlayerInput.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    // input variable
+    private InputSystem_Actions inputActions = null;
+
+    ////// publically readable variables for getting input ///////////
+    /// NOTE: please use these
+    // move and look vectors
+    public Vector2 movementInput = Vector2.zero;
+    public Vector2 lookInput = Vector2.zero;
+
+    // these variables are true when the player is HOLDING sprint or crouch
+    public bool sprintInput = false;
+    public bool crouchInput = false;
+
+    ////////
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    ////// when enabled, activate inputs
+    private void OnEnable()
+    {
+        inputActions.Enable();
+
+        inputActions.Player.Move.performed += InputOnMove;
+        inputActions.Player.Look.performed += InputOnLook;
+
+        inputActions.Player.Attack.performed += InputAttack;
+        inputActions.Player.Interact.performed += InputInteract;
+
+        inputActions.Player.Crouch.performed += InputCrouch;
+        inputActions.Player.Crouch.canceled += InputCrouch;
+        inputActions.Player.Jump.performed += InputJump;
+        inputActions.Player.Sprint.performed += InputSprint;
+        inputActions.Player.Sprint.canceled += InputSprint;
+
+        // can be "performed" or "canceled"
+        // performed = pressed down. canceled = released input
+    }
+
+    ////// when disabled, deactivate inputs
+    private void OnDisable() {
+        inputActions.Disable();
+
+        inputActions.Player.Move.performed -= InputOnMove;
+        inputActions.Player.Look.performed -= InputOnLook;
+
+        inputActions.Player.Attack.performed -= InputAttack;
+        inputActions.Player.Interact.performed -= InputInteract;
+
+        inputActions.Player.Crouch.performed -= InputCrouch;
+        inputActions.Player.Crouch.canceled -= InputCrouch;
+        inputActions.Player.Jump.performed -= InputJump;
+        inputActions.Player.Sprint.performed -= InputSprint;
+        inputActions.Player.Sprint.canceled -= InputSprint;
+    }
+
+    ////////////// input methods. Performed When inputing stuff ////////////
+    private void InputOnMove(InputAction.CallbackContext callbackValue) {
+        movementInput = callbackValue.ReadValue<Vector2>();
+    }
+
+    private void InputOnLook(InputAction.CallbackContext callbackValue) {
+        lookInput = callbackValue.ReadValue<Vector2>();
+    }
+
+
+    //// jump, crouch, sprint inputs
+    private void InputJump(InputAction.CallbackContext callbackValue) {
+        // call something to jump (here)
+    }
+    private void InputCrouch(InputAction.CallbackContext callbackValue) {
+        if (callbackValue.performed) { // player is holding down crouch
+            crouchInput = true;
+        } else if (callbackValue.canceled) { // player let go of crouch
+            crouchInput = false;
+        }
+    }
+    private void InputSprint(InputAction.CallbackContext callbackValue) {
+        if (callbackValue.performed) { // player is holding down sprint
+            sprintInput = true;
+        } else if (callbackValue.canceled) { // player let go of sprint
+            sprintInput = false;
+        }
+    }
+
+
+    //// interact, attack inputs
+    private void InputInteract(InputAction.CallbackContext callbackValue) {
+        // call something to interact (here)
+    }
+
+    private void InputAttack(InputAction.CallbackContext callbackValue) {
+        // call something to attack (here)
+    }
+}
