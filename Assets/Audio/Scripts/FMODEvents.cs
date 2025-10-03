f0b94f34142b238b7e534a81cdbbf683e22a4991
Brian Ma <=>
2025-10-02 23:14:55 -0400

    got footsteps sounds working kinda
    sounds instances need to have 3d attributes set for 3d instances

--- Assets/Audio/Scripts/FMODEvents.cs
+++ Assets/Audio/Scripts/FMODEvents.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 using FMODUnity;
 using System.Collections.Generic;
 
-[System.Serializable]
 public class FMODEvents : MonoBehaviour
 {
     [field: SerializeField] public EventReference enemyDeath { get; private set; }
+    [field: Header("Player SFX")] 
     [field: SerializeField] public EventReference footsteps { get; private set; }
+
+    [field: SerializeField] public List<EventReference> sounds { get; private set; }
+
     public static FMODEvents instance { get; private set; }
 
     private void Awake()
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMOD.Studio;
+using FMOD;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
@@ -51,7 +52,14 @@ public class PlayerMovement : MonoBehaviour
     {
         if (rb.linearVelocity.magnitude != 0)
         {
-            Debug.Log("can play sound");
+            // NOTE: 3d attributes need to be set in order to play instances in 3d
+            VECTOR pos = new VECTOR { x = rb.position.x, y = rb.position.y, z = rb.position.z };
+            VECTOR vel = new VECTOR { x = rb.linearVelocity.x, y = rb.linearVelocity.y, z = rb.linearVelocity.z };
+            VECTOR forward = new VECTOR { x = rb.linearVelocity.x / rb.linearVelocity.magnitude, y = rb.linearVelocity.y / rb.linearVelocity.magnitude, z = rb.linearVelocity.z / rb.linearVelocity.magnitude };
+            VECTOR up = new VECTOR { x = 0, y = 1, z = 0 };
+            ATTRIBUTES_3D attr = new ATTRIBUTES_3D { position = pos, velocity = vel, forward = forward, up = up };
+            footsteps.set3DAttributes(attr);
+
             PLAYBACK_STATE playbackState;
             footsteps.getPlaybackState(out playbackState);
 
