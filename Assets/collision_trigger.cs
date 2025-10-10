a6ceafd6a2543b06e91c3ba66872222aa00b92c2
SinnedFiend <231365677+SinnedFiend@users.noreply.github.com>
2025-10-10 11:46:07 -0400

    Changed the particles facing direction
    The particles now face away from the camera via a variable. This variable can be changed so it faces towards the camera/player.

--- Assets/collision_trigger.cs
+++ Assets/collision_trigger.cs
@@ -14,6 +14,7 @@ public class collision_trigger : MonoBehaviour
     private Vector3 displacementVector;
     private Transform playerCameraTransform;
     private Vector3 collisionEnterPosition;
+    public bool particleFaceDirection = true;
     private void OnTriggerEnter(Collider other)
     {
         collisionEnterPosition = other.transform.position;
@@ -23,32 +24,60 @@ public class collision_trigger : MonoBehaviour
             
             //Gets a reference to the players Camera.
             playerCameraTransform = other.transform.GetChild(0);
-            
+
             //This is how far away the particle system is placed. This is in addition to the forward vector of the camera which already has some numbers to it.
-            displacementVector = new Vector3(displacementNum, 0, displacementNum);
-            
-            Vector3 trueDirection = playerCameraTransform.forward;
+            //We will need these stuff when account for what direction the camera is truly facing in. Working with only the camera's forward motion will not get us anywhere as we need it's rotation as well.
+            float angle;
+            Vector3 axis;
+            playerCameraTransform.rotation.ToAngleAxis(out angle, out axis);
+            if(particleFaceDirection == false)
+            {
+                displacementVector = new Vector3(displacementNum, 0, displacementNum);
+            }
+            else
+            {
+                displacementVector = new Vector3(-displacementNum, 0, -displacementNum);
+            }
+            //We do this so that it reduces the distance at which the particle is placed.
+            displacementVector = -displacementVector;
+            //When we have the angle + forward vector of the camera (which I think is the forward vector of the player body) it becomes the true direction the camera is facing to.
+            Vector3 trueDirection = playerCameraTransform.forward * angle;
             
             //So that we don't account for sky particles. 
             trueDirection.y = 0;
             
             //This is so that we aren't outside of the field of view or are placed in an awkward position. Optimally it should (almost) face straight ahead at the player.
-            if(trueDirection.z > trueDirection.x)
+            /*if(trueDirection.z > trueDirection.x)
             {
                 displacementVector.x = 0;
             }
             else
             {
                 displacementVector.z = 0;
-            }
+            }*/
 
+            if(trueDirection.z < 0)
+            {
+                displacementVector.z = -displacementVector.z;
+            }
+            else if (trueDirection.x < 0)
+            {
+                displacementVector.x = -displacementVector.x;
+            }
             trueDirection += displacementVector;
 
             addingParticles.transform.position = trueDirection ;
             
-            //This gets the distance between the player position and the particle position, the thing we're rotating.
-            differencePos = collisionEnterPosition - addingParticles.transform.position;
-            
+            //This gets the distance between the player position and the particle position, the thing we're rotating. One of the if statements rotates it to face the camera while the other away from it.
+            if (particleFaceDirection == false)
+            {
+                differencePos = collisionEnterPosition - addingParticles.transform.position;
+            }
+            else
+            {
+                differencePos =  addingParticles.transform.position - collisionEnterPosition;
+            }
+
             faceAnotherObject = new Quaternion();
             faceAnotherObject = Quaternion.LookRotation(differencePos);
 
