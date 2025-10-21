adb18617fe32a56f84277cccb7f05b56d3408f6b
Squidking11 <nicobutler4854@gmail.com>
2025-10-20 20:26:15 -0400

    Began work on Audio Log System
    Created a Scriptable object that will manager starting audio plays. Added a .txt file that could hold dialog information that will be read by the Scriptable object.

---
+++ Assets/ScriptableObjects/AudioLogSystem/AudioLogObject.cs
@@ -0,0 +1,10 @@
+using System.IO;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioLogObject", menuName = "Scriptable Objects/AudioLogObject")]
+public class AudioLogObject : ScriptableObject
+{
+
+}
