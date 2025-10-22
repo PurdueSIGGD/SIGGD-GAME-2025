5e93dc45452587225fcce67b78683fc69c9dd1f2
Fluffbird119 <jaysanthony@gmail.com>
2025-10-22 16:14:10 -0400

    inital setup

--- Assets/Audio/Scripts/FMODEvents.cs
+++ Assets/Audio/Scripts/FMODEvents.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using FMODUnity;
 using System.Collections.Generic;
 using System;
+using System.Collections;
 
 public class FMODEvents : MonoBehaviour
 {
@@ -23,7 +24,9 @@ public class FMODEvents : MonoBehaviour
 
     public static FMODEvents instance { get; private set; }
 
-    [field: SerializeField] public static StringEventReferenceDictionary referenceDict = new StringEventReferenceDictionary();
+    public List<string> bankNames = new List<string>();
+
+    public Dictionary<string, EventReference> soundEvents = new Dictionary<string, EventReference>();
 
     private void Awake()
     {
@@ -34,5 +37,49 @@ public class FMODEvents : MonoBehaviour
         }
 
         instance = this;
+
+        foreach (string name in bankNames)
+        {
+            RuntimeManager.LoadBank(name, true); // the true forces them to all load at once
+        }
+
+        StartCoroutine(loadEventsPostBankLoading());
+    }
+
+    private IEnumerator loadEventsPostBankLoading()
+    {
+        yield return null; // it loads the banks early if you remove this (i have no clue as to why)
+
+        // the waiting code
+        while (RuntimeManager.AnySampleDataLoading())
+        {
+            yield return null;
+        }
+
+        // The actual code
+        foreach (string name in bankNames)
+        {
+            // removing .bank from the inital string given and adding bank:/ cuz its needed for the path to the bank
+            string filePath = "bank:/" + name.Replace(".bank", "");
+            FMOD.Studio.Bank bank;
+
+            // actually getting the bank after theyve been loaded
+            RuntimeManager.StudioSystem.getBank(filePath, out bank);
+
+            bank.getEventList(out FMOD.Studio.EventDescription[] eventDescriptions);
+
+            foreach (FMOD.Studio.EventDescription description in eventDescriptions)
+            {
+                description.getPath(out string eventPath);
+
+                EventReference eventRef = EventReference.Find(eventPath);
+                Debug.Log("eventPath is: " + eventPath);
+                Debug.Log("eventRef is: " + eventRef);
+
+                soundEvents.Add(eventPath.Replace("event:/", ""), eventRef); // the replace just makes the names a little nicer
+            }
+        }
+
+        Debug.Log("All " + soundEvents.Count + " events loaded");
     }
 }
