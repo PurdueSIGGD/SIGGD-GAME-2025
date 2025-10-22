fbda7bdf4586475479f8ddda12e5bdb1052dffc7
Fluffbird119 <jaysanthony@gmail.com>
2025-10-22 15:58:51 -0400

    banks stopped working fix

--- Assets/Audio/Scripts/FMODEvents.cs
+++ Assets/Audio/Scripts/FMODEvents.cs
@@ -18,11 +18,11 @@ public class FMODEvents : SerializedMonoBehaviour
 {
     public static FMODEvents instance { get; private set; }
 
-    [HideInInspector] public Dictionary<string, EventReference> soundEvents = new Dictionary<string, EventReference>(); // reference this when calling for a sound FMODEvents.soundEvents["name of sound"]
+    public Dictionary<string, EventReference> soundEvents = new Dictionary<string, EventReference>(); // reference this when calling for a sound FMODEvents.soundEvents["name of sound"]
 
-    public List<eventRefs> eventRefsList = new List<eventRefs>(); // add the events and name of the events to this list
+    //public List<eventRefs> eventRefsList = new List<eventRefs>(); // add the events and name of the events to this list
 
-    private List<string> bankNames = new List<string>(); // add bank names to this list
+    public List<string> bankNames = new List<string>(); // add bank names to this list
 
     private void Awake()
     {
@@ -39,16 +39,19 @@ public class FMODEvents : SerializedMonoBehaviour
             RuntimeManager.LoadBank(name, true); // the true forces them to all load at once
         }
 
+        loadEventsPostBankLoading();
+        /*
         // loop that adds all values from the list into the dictionary
         foreach (var eventRefs in eventRefsList)
         {
             soundEvents.Add(eventRefs.name, eventRefs.reference);
         }
+        */
     }
 
     // the name kinda says it all this is made to wait until all of FMOD is done loading before trying to make event references
     // DONT DELETE THIS YET - jay
-    /*
+    
     private IEnumerator loadEventsPostBankLoading()
     {
         yield return null; // it loads the banks early if you remove this (i have no F*cking clue as to why)
@@ -83,5 +86,5 @@ public class FMODEvents : SerializedMonoBehaviour
         }
 
         Debug.Log("All " + soundEvents.Count + " events loaded");
-    }*/
+    }
 }
