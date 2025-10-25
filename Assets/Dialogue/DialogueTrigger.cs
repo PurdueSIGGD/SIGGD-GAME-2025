6c101c319eb95148c935d1358b04c8d829bdbae7
Closetwings <Dilbeckmason@gmail.com>
2025-10-24 20:02:25 -0400

    Basic System
    Goal to update system to be better and more scalable, using a scriptable object

---
+++ Assets/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueCharacter
+{
+    public string name;
+    //public Sprite icon;
+}
+
+[System.Serializable]
+public class DialogueLine
+{
+    public DialogueCharacter character;
+    [TextArea(3, 10)]
+    public string line;
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    public List<DialogueLine> dialogueLines = new List<DialogueLine>();
+}
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public bool ExampleActivation = false;
+
+
+
+    public void TriggerDialogue()
+    {
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+
+    // Example of triggering dialogue on player collision
+    /*
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            TriggerDialogue();
+        }
+    }
+    */
+
+    void Update()
+    {
+
+        if (ExampleActivation) {
+            TriggerDialogue();
+            ExampleActivation = false; 
+        }
+        
+    }
+}
