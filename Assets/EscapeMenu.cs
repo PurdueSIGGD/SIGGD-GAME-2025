fcce88802b9e8aad823a2fd5c1740a91c063fe9d
WangXin1507 <simonwang421@outlook.com>
2025-10-26 22:59:03 -0400

    clean up

--- Assets/EscapeMenu.cs
+++ Assets/EscapeMenu.cs
@@ -4,37 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class EscapeMenu : MonoBehaviour
 {
-    private Button quitButton;
-    public GameObject buttonObj;
-    public GameObject buttonObject;
-    public GameObject fadedScreen;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private Canvas canvas;
     private bool isEnabled;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        if (buttonObj != null)
-        {
-            quitButton = buttonObj.GetComponent<Button>();
-            quitButton.onClick.AddListener(loadMainMenu);
-        }
-        buttonObject.SetActive(false);
-        fadedScreen.SetActive(false);
-        isEnabled = false;
+        quitButton.onClick.AddListener(loadMainMenu);
+        canvas.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             isEnabled = !isEnabled;
-            buttonObject.SetActive(isEnabled);
-            fadedScreen.SetActive(isEnabled);
+            canvas.enabled = isEnabled;
         }
     }
     public void loadMainMenu()
     {
-      
         SceneManager.LoadScene("Main Menu");
     }
 }
