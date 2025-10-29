79b9066263c06c48f30adf33b6c75f857ee437d3
Callista Kendall <callistajkendall@gmail.com>
2025-10-29 13:50:01 -0400

    hunger stuff

--- Assets/ScriptableObjects/Stats System/GenericStatPickup.cs
+++ Assets/ScriptableObjects/Stats System/GenericStatPickup.cs
@@ -21,6 +21,8 @@ public class GenericStatPickup : MonoBehaviour
             stats.ApplyMultiplier(statToAffect, multiplier, duration);
             Destroy(gameObject);
         }
+
+         
     }
 
 
--- Assets/Scripts/Resources/PlayerStats.cs
+++ Assets/Scripts/Resources/PlayerStats.cs
@@ -7,6 +7,8 @@ public class PlayerStats : MonoBehaviour
     public float currentHunger;
     public float hungerDecayRate = 1f;
 
+[SerializeField] private Stat statManager;
+
     private float hungerDamageTimer = 0f;     // tracks time since last starvation tick
     [SerializeField] private float hungerDamageInterval = 10f; // seconds between starvation damage
 
@@ -18,22 +20,53 @@ public class PlayerStats : MonoBehaviour
     {
         currentHunger = maxHunger;
         playerHealth = GetComponent<EntityHealthManager>();
+
+        statManager.OnStatChanged.AddListener(OnStatChanged);
+
+        
+    }
+
+    private void OnStatChanged(StatType stat, float value)
+    {
+        if (stat == StatType.maxHunger)
+        {
+            SetmaxHunger(value);
+            
+        }
+
+        if (stat == StatType.hungerDecayRate)
+        {
+            SethungerDecayRate(value);
+        }
+    }
+
+    public void SetmaxHunger(float newmaxHunger)
+    {
+        maxHunger = newmaxHunger;
+    }
+    public void SethungerDecayRate(float newhungerDecayRate)
+    {
+        hungerDecayRate = newhungerDecayRate;
     }
 
     void Update()
     {
+        //Debug.Log(currentHunger);
+        //Debug.Log(hungerDecayRate);
         //hunger goes down and takes health when starving
         if (currentHunger > 0)
         {
             currentHunger -= hungerDecayRate * Time.deltaTime;
             hungerDamageTimer = 0f; // Reset timer if not starving
 
-            // Debug.Log(currentHunger);
+         
         } 
+
+
         else 
         {
             //player is starving stuff
-            // Debug.Log("Starving")
+            //Debug.Log("Starving");
  
             hungerDamageTimer += Time.deltaTime;
 
