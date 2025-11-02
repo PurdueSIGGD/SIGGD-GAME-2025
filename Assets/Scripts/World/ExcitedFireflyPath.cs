1ca9923108779a99d09f53f472530e12d676226f
aayushrh <73722373+aayushrh@users.noreply.github.com>
2025-11-02 14:35:12 -0500

    SKYBOX AND CAVE

--- Assets/Scripts/World/ExcitedFireflyPath.cs
+++ Assets/Scripts/World/ExcitedFireflyPath.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ExcitedFireflyPath : MonoBehaviour
 {
@@ -14,6 +15,21 @@ public class ExcitedFireflyPath : MonoBehaviour
     private Vector3 prev;
     private Vector3 start;
     private int index = 0;
+    private List<Vector3> goPathVec = new List<Vector3>();
+    private List<Vector3> exitPathVec = new List<Vector3>();
+
+    private void Start()
+    {
+        start = transform.position;
+        for(int i = 0; i < goPath.transform.childCount; i++)
+        {
+            goPathVec.Add(goPath.transform.GetChild(i).position);
+        }
+        for (int i = 0; i < exitPath.transform.childCount; i++)
+        {
+            exitPathVec.Add(exitPath.transform.GetChild(i).position);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -24,7 +40,7 @@ public class ExcitedFireflyPath : MonoBehaviour
             index = 0;
             GetComponent<SphereCollider>().enabled = false;
             prev = start;
-            next = goPath.transform.GetChild(index).position;
+            next = goPathVec[index];
             Debug.Log("Excited Firefly Path Activated");
         }
     }
@@ -43,8 +59,8 @@ public class ExcitedFireflyPath : MonoBehaviour
         if (going)
         {
             timer += Time.deltaTime;
-            transform.position = LerpVector(prev, next, timer);
-            if (timer >= 1f)
+            transform.position = LerpVector(prev, next, timer/(16.0f/goPathVec.Count));
+            if (timer >= 16f/(goPathVec.Count))
             {
                 timer = 0.0f;
                 if (index == goPath.transform.childCount - 1)
@@ -53,22 +69,22 @@ public class ExcitedFireflyPath : MonoBehaviour
                     exiting = true;
                     index = 0;
                     prev = transform.position;
-                    next = exitPath.transform.GetChild(index).position;
+                    next = exitPathVec[index];
                     Debug.Log("Excited Firefly Exiting");
                 }
                 else
                 {
                     index++;
                     prev = next;
-                    next = goPath.transform.GetChild(index).position;
+                    next = goPathVec[index];
                 }
             }
         }
-        else if (going)
+        else if (exiting)
         {
             timer += Time.deltaTime;
-            transform.position = LerpVector(prev, next, timer);
-            if (timer >= 1f)
+            transform.position = LerpVector(prev, next, timer/16.0f);
+            if (timer >= 16f)
             {
                 timer = 0.0f;
                 if (index == goPath.transform.childCount - 1)
@@ -83,7 +99,7 @@ public class ExcitedFireflyPath : MonoBehaviour
                 {
                     index++;
                     prev = next;
-                    next = goPath.transform.GetChild(index).position;
+                    next = goPathVec[index];
                 }
             }
         }
