bdbadf4a1d2d6c2820dd527cffe10abb62df25a7
xinmew <60266027+WangXin1507@users.noreply.github.com>
2025-12-07 21:08:33 -0500

    game start save set up

---
+++ Assets/Scripts/Save/SaveModules/GameProgressDataSaveModule.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameProgressDataSaveModule : ISaveModule
+{
+    public bool serialize()
+    {
+        return true;
+    }
+
+    public bool deserialize()
+    {
+        return true;
+    }
+}
