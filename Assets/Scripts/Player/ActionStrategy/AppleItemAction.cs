b1c443ba7eda85fbb898cc9fb5b4bb565213fab6
penguin3145 <ryananam3145@gmail.com>
2026-02-09 19:36:49 -0500

    Posion mushrooms

--- Assets/Scripts/Player/ActionStrategy/AppleItemAction.cs
+++ Assets/Scripts/Player/ActionStrategy/AppleItemAction.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public class AppleItem : IPlayerActionStrategy
 {
-
     protected override void OnEnter()
     {
         base.OnEnter();
+        Inventory.Instance.Decrement();
         DamageContext healContext = new DamageContext();
         healContext.attacker = healContext.victim = PlayerID.Instance.gameObject;
         healContext.amount = 20;
         PlayerID.Instance.GetComponent<EntityHealthManager>().Heal(healContext);
         PlayerID.Instance.GetComponent<PlayerHunger>().UpdateHunger(20);
-        Inventory.Instance.Decrement();
         Debug.Log("player ate an apple");
     }
 }
