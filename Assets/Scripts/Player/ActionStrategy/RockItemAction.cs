6c30c970b7fa941388b16782fdb21dea6038cc08
Squidking11 <nicobutler4854@gmail.com>
2026-02-23 20:43:25 -0500

    Animated Rock Idle

--- Assets/Scripts/Player/ActionStrategy/RockItemAction.cs
+++ Assets/Scripts/Player/ActionStrategy/RockItemAction.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class RockItemAction : IPlayerActionStrategy
 {
+    private Camera playerCam;
     protected override void OnEnter()
     {
+        playerCam = PlayerID.Instance.cam.GetComponentInChildren<Camera>();
+        Debug.Log("Player is facing " + playerCam.transform.forward);
+
+
         base.OnEnter();
-        PlayHandAction(); // plays animation for apple, but this is instant rn so it does nothing
+        PlayHandAction(); // plays animation
         Inventory.Instance.Decrement();
         Debug.Log("player threw a rock");
+
     }
 }
