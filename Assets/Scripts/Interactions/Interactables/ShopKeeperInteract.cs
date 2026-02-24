d8ebc5b57a3539a840844d6cda6a37a67392a604
Joshua-Lindquist <lindquj@purdue.edu>
2026-02-23 22:21:21 -0500

    Updated villager scripts, attempted to add villager movement.

--- Assets/Scripts/Interactions/Interactables/ShopKeeperInteract.cs
+++ Assets/Scripts/Interactions/Interactables/ShopKeeperInteract.cs
@@ -2,27 +2,20 @@ using UnityEngine;
 
 public class ShopKeeperInteract : MonoBehaviour, IInteractable<IInteractor>
 {
-    private InteractableUI ui;
-    public void OnHoverEnter(InteractableUI ui)
-    {
-        if (CraftingMenu.Instance.IsCanvasActive())
-        {
-            return;
-        }
 
-        ui.ActivateUI(this);
-        this.ui = ui;
-    }
-
-    public void OnHoverExit(InteractableUI ui)
-    {
-        ui.DeactivateUI();
-        this.ui = ui;
-    }
+    // For now the shopkeeper script is just opening the crafting menu since the shopkeeper
+    // doesn't need to move (and I am unsure if it should also rotate to face the player or
+    // if it stays completely stationary).
+    public void OnHoverEnter(InteractableUI ui) { }
+    public void OnHoverExit(InteractableUI ui) { }
 
     public void OnInteract(IInteractor interactor)
     {
-        ui.DeactivateUI();
-        CraftingMenu.Instance.ShowCraftingMenu(true);
+        if (CraftingMenu.Instance)
+            CraftingMenu.Instance.ShowCraftingMenu(true);
+        else
+            Debug.LogError("ShopKeeper: CraftingMenu instance not found!");
     }
+
+
 }
--- Assets/Scripts/Interactions/Interactables/VillagerInteract.cs
+++ Assets/Scripts/Interactions/Interactables/VillagerInteract.cs
@@ -3,20 +3,37 @@ using UnityEngine;
 public class VillagerInteract : MonoBehaviour, IInteractable<IInteractor>
 {
     InteractableUI ui;
+    [SerializeField] private float lookSpeed = 5f;
+
+    private bool playerIsLooking = false;
+    private Transform playerTransform;
+
+    private void Update()
+    {
+        // Smoothly rotate toward the player while they are looking at us and holding a flower
+        if (playerIsLooking && playerTransform != null && PlayerIsHoldingFlower())
+        {
+            Vector3 direction = playerTransform.position - transform.position;
+            direction.y = 0f;
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRot = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * lookSpeed);
+            }
+        }
+    }
 
     public void OnHoverEnter(InteractableUI ui) {
-        if (CraftingMenu.Instance.IsCanvasActive())
+        playerIsLooking = true;
+        if (PlayerID.Instance != null)
         {
-            return;
+            playerTransform = PlayerID.Instance.transform;
         }
-
-        ui.ActivateUI(this);
-        this.ui = ui;
     }
 
     public void OnHoverExit(InteractableUI ui) {
-        ui.DeactivateUI();
-        this.ui = ui;
+        playerIsLooking = false;
+        playerTransform = null;
     }
 
     public void OnInteract(IInteractor interactor)
@@ -28,5 +45,10 @@ public class VillagerInteract : MonoBehaviour, IInteractable<IInteractor>
             ItemInfo slimeball = RecipeInfo.Instance.NamesToItemInfos[ItemInfo.ItemName.Slimeball];
             Inventory.Instance.AddItem(slimeball, 1);
         }
+    }
+    private bool PlayerIsHoldingFlower()
+    {
+        var selectedItem = Inventory.Instance.GetSelectedItem();
+        return selectedItem != null && selectedItem.itemName == ItemInfo.ItemName.Flower;
     }
 }
