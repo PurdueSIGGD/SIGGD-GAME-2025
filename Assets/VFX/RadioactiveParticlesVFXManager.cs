97add91c47ea0293ccbe88ced0d801efb3652bb8
Prisha Bangera <prishasbangera@gmail.com>
2026-02-25 20:50:26 -0500

    Started working on tweaking parameters and adding a Radioactive Particles VFX manager script

---
+++ Assets/VFX/RadioactiveParticlesVFXManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class RadioactiveParticlesVFXManager : MonoBehaviour
+{
+
+    VisualEffect particlesVFX = null;
+
+    private void Awake()
+    {
+        particlesVFX = GetComponent<VisualEffect>();
+    }
+    void OnPlay()
+    {
+        particlesVFX.Play();
+    }
+
+    void OnStop()
+    {
+        particlesVFX?.Stop();
+    }
+
+    
+}
