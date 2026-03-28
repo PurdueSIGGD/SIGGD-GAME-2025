f7b905a987dbcced142f3adf1f97626f23677fa9
Squidking11 <nicobutler4854@gmail.com>
2026-03-27 20:04:48 -0400

    Finished Bait
    Added despawn timer and fixed collision

--- Assets/ActivateBait.cs
+++ Assets/ActivateBait.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ActivateBait : MonoBehaviour
 {
+    // Radius and Duration both set on each bait types item info prefab
     public float radius = 10f;
     public float duration = 10f;
 
@@ -14,35 +16,34 @@ public class ActivateBait : MonoBehaviour
     {
         this.radius = radius;
         this.duration = duration;
-        //Debug.Log($"Bait initialized with radius: {radius} and duration: {duration}");
     }
 
     // Triggers when the bait hits the ground. Creates trigger sphere that checks for mobs within the radius
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            return; // Ignore collisions with the player
+        }
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // Rotate the bait to lay flat on the ground
         Destroy(GetComponent<Rigidbody>()); // Remove the Rigidbody to stop the bait from moving after it hits the ground
         Destroy(GetComponent<BoxCollider>());
-        
-        
-        //Collider[] hits = Physics.OverlapSphere(transform.position, radius, mask);
-        //if (hits.Length > 0)
-        //{
-        //    foreach (Collider hit in hits)
-        //    {
-        //        Debug.Log("Mob Detected");
-        //        /* This should call the mob code that tells them to move to this objects location
-        //           Durration should be passed to the mob to know how long they should be attracted to the bait
-        //           After the duration is completed the bait should be destroyed */
-        //    }
-        //}
+        StartCoroutine(DestroyAfterDuration());
     }
+
     void OnTriggerEnter(Collider other)
     {
 
         if ((mask.value & (1 << other.gameObject.layer)) != 0)
         {
-            Debug.Log("Mob detected");
             // This should call the mob code that tells them to move to this objects location
         }
+    }
+
+    private IEnumerator DestroyAfterDuration()
+    {
+        yield return new WaitForSeconds(duration); // Wait for the specified duration
+        // If needed , you can add code here to notify mobs that the bait is no longer active before destroying it
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player/ActionStrategy/ThrowBaitAction.cs
+++ Assets/Scripts/Player/ActionStrategy/ThrowBaitAction.cs
@@ -6,7 +6,9 @@ public class ThrowBaitAction : IPlayerActionStrategy
     public GameObject projectile;
     [Tooltip("Force the projectile will be thrown with")]
     public float throwForce = 15f;
+    [Tooltip("Radius of the bait's effect")]
     public float radius = 10f;
+    [Tooltip("Duration of the bait's effect in seconds")]
     public float duration = 5f;
     protected override void OnEnter()
     {
