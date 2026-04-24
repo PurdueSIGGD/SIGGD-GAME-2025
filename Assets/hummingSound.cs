2c21138d99f52b5b8eb51d46fb843a581beece01
nathanWhitacre <whitacre@purdue.edu>
2026-04-24 00:14:20 -0400

    right so why is the music not playing?

---
+++ Assets/hummingSound.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class HummingSound : ExternalEventTriggerer
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void TriggerExternalEvent()
+    {
+        Debug.Log("Triggered Music Time");
+        MusicManager.Instance.CrossFadeMusic("ForestAmbienceFirst", 0.1f);
+        PlayerID.Instance.GetComponent<PlayerHummingSound>().StopHumming();
+    }
+}
