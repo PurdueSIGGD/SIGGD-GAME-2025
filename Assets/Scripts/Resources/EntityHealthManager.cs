d77dfb38fd43b24356d67ff6aec7766f40367c47
Fluffbird119 <jaysanthony@gmail.com>
2026-04-24 06:39:23 -0400

    throp

--- Assets/Scripts/Resources/EntityHealthManager.cs
+++ Assets/Scripts/Resources/EntityHealthManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using FMODUnity;
 
 public class EntityHealthManager : StatProvider, IHealth
 {
@@ -44,6 +45,19 @@ public class EntityHealthManager : StatProvider, IHealth
         {
             Die(damageContext);
         }
+
+        if (gameObject == PlayerID.Instance.gameObject)
+        {
+            // damage noises
+            if (damageContext.amount >= 15)
+            {
+                RuntimeManager.PlayOneShot(FMODEvents.Instance.GetEventReferenceNoAsync("HeavyDamage"), PlayerID.Instance.transform.position);
+            }
+            else
+            {
+                RuntimeManager.PlayOneShot(FMODEvents.Instance.GetEventReferenceNoAsync("LightDamage"), PlayerID.Instance.transform.position);
+            }
+        }
     }
 
     public void Heal(DamageContext healContext)
@@ -82,7 +96,7 @@ public class EntityHealthManager : StatProvider, IHealth
             // Change state of player to peaceful
 
             if (GameStateManager.Instance) GameStateManager.Instance.attemptSetState(GameStateManager.GameState.PEACEFUL, gameObject);
-            AudioManager.Instance.PlayOneShotNoAsync(playerDeathSound, PlayerID.Instance.gameObject.transform.position);
+            RuntimeManager.PlayOneShot(FMODEvents.Instance.GetEventReferenceNoAsync("Death"), PlayerID.Instance.transform.position);
         }
     }
 
