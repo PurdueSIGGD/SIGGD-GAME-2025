5e12782a550f8b2eeef507811ae21e9a94117b41
nathanWhitacre <whitacre@purdue.edu>
2026-04-24 10:08:28 -0400

    settingsMenutoPrefab

--- Assets/Scripts/Player/ActionStrategy/PurpleMushroomAction.cs
+++ Assets/Scripts/Player/ActionStrategy/PurpleMushroomAction.cs
@@ -22,6 +22,5 @@ public class PurpleMushroomItemAction : IPlayerActionStrategy
         PlayerID.Instance.GetComponent<EntityHealthManager>().Heal(healContext);
         PlayerID.Instance.GetComponent<PlayerHunger>().UpdateHunger(20);
         Debug.Log("player ate a mushroom");
-        yield return null;
     }
 }
