5509f7c14ddfd287607015cb9c046d89d70607b4
Jasen <jspeak10@gmail.com>
2026-08-04 15:16:55 -0400

    trying to make a telescope for fun
    wow

--- Assets/Scripts/Player/ActionStrategy/AppleItemAction.cs
+++ Assets/Scripts/Player/ActionStrategy/AppleItemAction.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 public class AppleItem : IPlayerActionStrategy
 {
     private static readonly string eatSound = "Eating";
+    private bool isOn = false;
+
+    private void OnEnable()
+    {
+        Inventory.OnHotbarSelectionChanged += OnHotbarSelectionChanged;
+    }
+
+    private void OnDisable()
+    {
+        Inventory.OnHotbarSelectionChanged -= OnHotbarSelectionChanged;
+    }
+
+
+
+
     protected override void OnEnter()
     {
         base.OnEnter();
         PlayHandAction(); // plays animation for apple, but this is instant rn so it does nothing
-        Inventory.Instance.Decrement();
+        //Inventory.Instance.Decrement();
         AudioManager.Instance.PlayOneShotNoAsync(eatSound, PlayerID.Instance.gameObject.transform.position);
         DamageContext healContext = new DamageContext();
         healContext.attacker = healContext.victim = PlayerID.Instance.gameObject;
@@ -15,5 +30,41 @@ public class AppleItem : IPlayerActionStrategy
         PlayerID.Instance.GetComponent<EntityHealthManager>().Heal(healContext);
         PlayerID.Instance.GetComponent<PlayerHunger>().UpdateHunger(20);
         Debug.Log("player ate an apple");
+        Debug.Log("jasen made a change");
+        //Camera.main.fieldOfView = 30;
+        
+        isOn = !isOn;
+        if (isOn)
+        {
+            Camera.main.fieldOfView = 30;
+        }
+        else
+        {
+            Camera.main.fieldOfView = 60;
+        }
+
     }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+        //Camera.main.fieldOfView = 30;
+    }
+
+    protected override void OnExit()
+    {
+        base.OnExit();
+        //Camera.main.fieldOfView = 60;
+
+
+    }
+
+    private void OnHotbarSelectionChanged(int selectedIndex)
+    {
+        // If the player switches to a different item while eating, exit the action state
+        Camera.main.fieldOfView = 60;
+    }
+
+
+
 }
