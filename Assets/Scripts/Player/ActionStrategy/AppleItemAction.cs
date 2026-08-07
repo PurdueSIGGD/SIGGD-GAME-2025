45e93aff7d07fb9e554b089af966ffcdd1c896a5
Jasen <jspeak10@gmail.com>
2026-08-07 01:51:27 -0400

    a  lotta things added

--- Assets/Scripts/Player/ActionStrategy/AppleItemAction.cs
+++ Assets/Scripts/Player/ActionStrategy/AppleItemAction.cs
@@ -5,16 +5,7 @@ public class AppleItem : IPlayerActionStrategy
     private static readonly string eatSound = "Eating";
     private bool isOn = false;
 
-    private void OnEnable()
-    {
-        Inventory.OnHotbarSelectionChanged += OnHotbarSelectionChanged;
-    }
-
-    private void OnDisable()
-    {
-        Inventory.OnHotbarSelectionChanged -= OnHotbarSelectionChanged;
-    }
-
+    
 
 
 
@@ -22,28 +13,18 @@ public class AppleItem : IPlayerActionStrategy
     {
         base.OnEnter();
         PlayHandAction(); // plays animation for apple, but this is instant rn so it does nothing
-        //Inventory.Instance.Decrement();
+        Inventory.Instance.Decrement();
         AudioManager.Instance.PlayOneShotNoAsync(eatSound, PlayerID.Instance.gameObject.transform.position);
         DamageContext healContext = new DamageContext();
         healContext.attacker = healContext.victim = PlayerID.Instance.gameObject;
-        healContext.amount = -20;
+        healContext.amount = -40;
         PlayerID.Instance.GetComponent<EntityHealthManager>().Heal(healContext);
         PlayerID.Instance.GetComponent<PlayerHunger>().UpdateHunger(20);
         Debug.Log("player ate an apple");
-        Debug.Log("jasen made a change");
-        //Camera.main.fieldOfView = 30;
+       
+        
 
 
-        float currentFOV = Camera.main.fieldOfView;
-
-        if (currentFOV > 40f)
-        {
-            CameraZoomController.Instance.SetFOV(30f);
-        }
-        else
-        {
-            CameraZoomController.Instance.SetFOV(60f);
-        }
     }
 
     protected override void OnUpdate()
@@ -60,11 +41,7 @@ public class AppleItem : IPlayerActionStrategy
 
     }
 
-    private void OnHotbarSelectionChanged(int selectedIndex)
-    {
-        // If the player switches to a different item while eating, exit the action state
-        Camera.main.fieldOfView = 60;
-    }
+   
 
 
 
