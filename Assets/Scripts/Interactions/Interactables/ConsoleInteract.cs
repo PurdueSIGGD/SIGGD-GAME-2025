65daeed863467f43b2e7019670109390111f6431
Aayush Harwadekar <73722373+aayushrh@users.noreply.github.com>
2026-08-17 22:33:20 -0700

    console made

---
+++ Assets/Scripts/Interactions/Interactables/ConsoleInteract.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ConsoleInteract : MonoBehaviour, IInteractable<IInteractor>
+{
+    public static int consolesBroken = 0;
+    
+    public static readonly string ConsoleBroken = "ConsoleDamaged";
+
+    public void OnHoverEnter(InteractableUI ui) {
+        ui.ActivateUI(this);
+    }
+    public void OnHoverExit(InteractableUI ui) {
+        ui.DeactivateUI();
+    }
+
+    public void OnInteract(IInteractor interactor)
+    {
+        Debug.Log("Broke this console");
+        AudioManager.Instance.PlayOneShotNoAsync(ConsoleBroken, transform.position);
+        Destroy(this.gameObject);
+    }
+}
