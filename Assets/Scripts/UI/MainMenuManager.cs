03f1b7b2b5f9beb9feae31b879aa094689c9ac86
Jasen <jspeak10@gmail.com>
2026-08-27 12:08:10 -0400

    Revert "Merge branch 'dev' into Aayush-FinalTouches"
    This reverts commit e1ca48767d0bafaf31476519acad178944b0f3c0, reversing
    changes made to 8efb342c4b3bd7fd0a15143b5358a0cba20507ad.

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible = true;
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
     public void ShowSettingsMenu()
     {
